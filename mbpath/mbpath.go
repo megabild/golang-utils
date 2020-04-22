b2f1a04345a9d21a4af73aa5ca4b06abe36a8ce9
Carsten Rothhaß <crothhass@megabild.de>
2020-04-22 10:40:06 +0200

    Fix fatal error with empty string in RemoveDriveLetter

--- mbpath/mbpath.go
+++ mbpath/mbpath.go
@@ -2,7 +2,7 @@ package mbpath
 
 // RemoveDriveLetter removes windows drive letter from mbstring
 func RemoveDriveLetter(s string) string {
-	if s[1] == ':' {
+	if len(s) > 1 && s[1] == ':' {
 		s = s[2:]
 	}
 	return s
